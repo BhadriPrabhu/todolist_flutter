4130c1c2319d9f66282911da2510e2329bbd4e0b
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2026-07-05 23:23:09 +0530

    feat: highlight the warning helper in add task for alarm disabled

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -959,10 +959,22 @@ class _HomeScreenState extends State<HomeScreen>
                 dialogState["dueDate"] != null
                     ? dialogState['alarm'] ?? false
                     : false,
-            onChanged: (value) {
+            onChanged: (value) async {
               if (dialogState["dueDate"] == null && value == true) {
                 showSnackBar('Please select a due date first');
                 debugPrint('Cannot enable alarm without due date');
+
+                setDialogState(() {
+                  dialogState['highlightWarning'] = true;
+                });
+
+                // 2. Wait for 300 milliseconds
+                await Future.delayed(const Duration(milliseconds: 300));
+
+                // 3. Revert back to normal state
+                setDialogState(() {
+                  dialogState['highlightWarning'] = false;
+                });
                 return;
               } else {
                 setDialogState(() {
@@ -980,13 +992,22 @@ class _HomeScreenState extends State<HomeScreen>
           dialogState["dueDate"] == null
               ? Align(
                 alignment: Alignment.centerLeft,
-                child: Text(
-                  'Set a due date to enable alarm',
+                child: AnimatedDefaultTextStyle(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
                   style: GoogleFonts.poppins(
-                    color: const Color.fromARGB(255, 229, 18, 68),
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
+                    color:
+                        (dialogState['highlightWarning'] ?? false)
+                            ? Colors.redAccent
+                            : const Color.fromARGB(255, 229, 18, 68),
+                    fontSize:
+                        (dialogState['highlightWarning'] ?? false) ? 14 : 12,
+                    fontWeight:
+                        (dialogState['highlightWarning'] ?? false)
+                            ? FontWeight.bold
+                            : FontWeight.w500,
                   ),
+                  child: const Text('Set a due date to enable alarm'),
                 ),
               )
               : const SizedBox.shrink(),
@@ -1938,19 +1959,21 @@ class _HomeScreenState extends State<HomeScreen>
                     }
                   },
                 ),
-                _sortBy == "Custom" ? ReorderableDragStartListener(
-                  index: index,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 4.0, right: 4.0),
-                    child: Icon(
-                      Icons.drag_handle,
-                      color:
-                          _isDarkTheme
-                              ? ThemeConfig.secondaryTextColor
-                              : Colors.black54,
-                    ),
-                  ),
-                ) : const SizedBox.shrink(),
+                _sortBy == "Custom"
+                    ? ReorderableDragStartListener(
+                      index: index,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+                        child: Icon(
+                          Icons.drag_handle,
+                          color:
+                              _isDarkTheme
+                                  ? ThemeConfig.secondaryTextColor
+                                  : Colors.black54,
+                        ),
+                      ),
+                    )
+                    : const SizedBox.shrink(),
               ],
             ),
             onTap: () {
